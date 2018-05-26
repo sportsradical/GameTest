3af002c30aa25571f58044f26c1b0318de1c1ebb
sportsradical <peereira@live.com.pt>
2018-05-26 22:07:37 +0100

    Add files via upload

---
+++ source/Common/BsExampleConfig.h
@@ -0,0 +1,4 @@
+#pragma once
+
+static constexpr const char* EXAMPLE_ROOT = "C:/Users/fiipe/Desktop/bsfExamples/"; 
+static constexpr const char* EXAMPLE_DATA_PATH = "C:/Users/fiipe/Desktop/bsfExamples/Data/"; 
